f984d1f5f1519d073a316542296a1dbaa1ba2b58
Baraa Hegazy <hegazyba@msu.edu>
2020-09-05 19:47:55 -0400

    Initial Commit

---
+++ tipperPresentationTests/tipperPresentationTests.swift
@@ -0,0 +1,34 @@
+//
+//  tipperPresentationTests.swift
+//  tipperPresentationTests
+//
+//  Created by Baraa Hegazy on 9/5/20.
+//  Copyright © 2020 Codepath. All rights reserved.
+//
+
+import XCTest
+@testable import tipperPresentation
+
+class tipperPresentationTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
